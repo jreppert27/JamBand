c735d08d427ab4afc6c31bf061cf16d606b7b540
jackr <jreppert@ithaca.edu>
2025-04-08 12:04:41 -0400

    adding basic things

---
+++ app/forms.py
@@ -0,0 +1,53 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_password(self, password):
+        user = db.session.scalar(sa.select(User).where(
+            User.password == password.data))
+        if user is not None:
+            raise ValidationError('Please use a different password.')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timezone
+from urllib.parse import urlsplit
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+import sqlalchemy as sa
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
+    EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
+from app.models import User, Post
+from app.email import send_password_reset_email
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template(index.html, title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
