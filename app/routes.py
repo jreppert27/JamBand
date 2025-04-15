327e5e96ac7df660ec9a9e431949a95e3fe6d225
gavin <ggarver@ithaca.edu>
2025-04-15 11:37:16 -0400

    updated index route

--- app/routes.py
+++ app/routes.py
@@ -13,22 +13,27 @@ from werkzeug.utils import secure_filename
 
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
-    if form.validate_on_submit():
+    if current_user.is_authenticated and form.validate_on_submit():
         post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
-    posts = db.paginate(current_user.following_posts(), page=page,
-                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('index', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
-        if posts.has_prev else None
+    if current_user.is_authenticated:
+        posts = db.paginate(current_user.following_posts(), page=page,
+                            per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    else:
+        posts = db.paginate(
+            sa.select(Post).order_by(Post.timestamp.desc()),
+            page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False
+        )
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html', title='Home', form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
